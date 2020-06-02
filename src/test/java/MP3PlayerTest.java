d5cdbbe6e0c0c7e76066359241707fa11da6bf0d
CrugBarat <craigtbarratt@gmail.com>
2020-06-02 18:37:10 +0100

    adds MP3Player class with constructor/getter/setter and connect method with tests - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/test/java/MP3PlayerTest.java
@@ -0,0 +1,41 @@
+import components.CDPlayer;
+import components.Radio;
+import components.RecordDeck;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MP3PlayerTest {
+    MP3Player mp3Player;
+    Stereo stereo;
+    Radio radio;
+    CDPlayer cdPlayer;
+    RecordDeck recordDeck;
+
+
+    @Before
+    public void before() {
+        mp3Player = new MP3Player("iPod", "Mini");
+        radio = new Radio("Sony","RD10928", 102.5);
+        cdPlayer = new CDPlayer("Sony", "XD12345", 4);
+        recordDeck = new RecordDeck("Sony","RP98278", 6);
+        stereo = new Stereo("Sony SoundSystem", radio, cdPlayer, recordDeck);
+    }
+
+    @Test
+    public void canGetMake() {
+        assertEquals("iPod", mp3Player.getMake());
+    }
+
+    @Test
+    public void canGetModel() {
+        assertEquals("Mini", mp3Player.getModel());
+    }
+
+    @Test
+    public void canConnectToStereo() {
+        assertEquals("Connected to: Sony SoundSystem",  mp3Player.connect(stereo));
+    }
+
+}
